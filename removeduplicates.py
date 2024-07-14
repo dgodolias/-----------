8c5e32464a85a2e87a013313333ff0b0751f7925
dgodolias <dgodolias@gmail.com>
2024-07-14 17:48:34 +0300

    the removeduplicates works ok for non colored cells

--- removeduplicates.py
+++ removeduplicates.py
@@ -61,20 +61,23 @@ def find_and_delete_duplicates(file_path, phone_column_name, mobile_column_name)
         name_cell = row[name_column_index - 1]  # Get the name cell
         phone_value_int = to_int(phone_cell.value)  # Convert phone value to int if possible
         mobile_value_int = to_int(mobile_cell.value)  # Convert mobile value to int if possible
+
+        combined_key = None
         
-        if phone_value_int in seen:
+        if phone_value_int is not None and phone_value_int in seen:
             combined_key = phone_value_int
-        elif mobile_value_int in seen:
+        elif mobile_value_int is not None and mobile_value_int in seen:
             combined_key = mobile_value_int
         else:
             combined_key = phone_value_int if phone_value_int is not None else mobile_value_int
-        
+
         if combined_key is not None:
             if combined_key in duplicates_info:
                 duplicates_info[combined_key].append((row[0].row, name_cell.value))
             else:
                 duplicates_info[combined_key] = [(row[0].row, name_cell.value)]
-            seen[combined_key] = row[0].row
+            seen[phone_value_int] = row[0].row
+            seen[mobile_value_int] = row[0].row
 
     if duplicates_info:
         print("Lists of duplicates based on 'Phone' or 'Mobile':")
